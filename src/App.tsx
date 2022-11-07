c14affb414690dda83d47befeb82474bc853eb80
Rahilsiddique <siddiquerahil19@gmail.com>
2022-11-07 23:43:16 +0530

    layout fixed with grid-row 2/-1 thing
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 function App() {
   return (
-    <main className="grid grid-rows-[repeat(10,_1fr)] bg-[#DFF6FF] w-screen h-screen">
+    <main className="grid grid-rows-[repeat(8,_1fr)] bg-[#DFF6FF] w-screen h-screen">
       <Navbar />
       <Main />
     </main>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,23 +6,22 @@ const Main = () => {
   const { settings } = useContext(SettingsContext);
 
   return (
-    // <section className="[grid-row:4/-1] sm:[grid-row:3/-1]">
-    <section className="[grid-row:4/-1] sm:[grid-row:3/-1] overflow-hidden border-black m-2 border-2">
-      <div className="flex w-full h-full ">
+    <section className="[grid-row:2/-1] sm:[grid-row:2/-1] overflow-hidden border-black m-2 border-2">
+      <div className="flex w-full h-full items-end">
         {items.map((item, idx) => (
           <div
             key={`${item}-${settings.arrayLen}-${idx}`}
             className="flex-1"
             style={{
               backgroundColor: "#1363DF",
-              height: `${item / 7}%`
+              height: `${item / 7}%`,
+              border: "1px solid #47B5FF"
             }}
             id={`${idx}`}
           />
         ))}
       </div>
     </section>
-    // {/* </section> */}
   );
 };
 
